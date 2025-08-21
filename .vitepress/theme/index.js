dd7b532f4abe42e38321142470836b9340071b13
hrsthrt74 <193000961@qq.com>
2025-08-22 02:10:54 +0800

    注释

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -3,8 +3,18 @@ import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
-import WFDownloadBtn from '../theme/components/watchface_download_button.vue'
 
+
+
+// 要加新组件就在这里导入
+import WFDownloadBtn from '../theme/components/watchface_download_button.vue'
+import Highlight from './components/format/highlight.vue'
+import Color from './components/format/color.vue'
+
+
+
+
+// 前面别动
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
@@ -13,8 +23,14 @@ export default {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
+
+
+  
+
   enhanceApp({ app, router, siteData }) {
-    // ...
-    app.component('WFDownloadBtn', WFDownloadBtn)
+    // 在这里导出
+    app.component('WFDownloadBtn', WFDownloadBtn);
+    app.component('Highlight', Highlight);
+    app.component('Color', Color)
   }
 }
