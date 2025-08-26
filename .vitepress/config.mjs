be5e19861353791ab96c2364cd18a0ec230f5789
hrsthrt74 <193000961@qq.com>
2025-08-26 13:26:22 +0800

    u got 2 heads

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -18,6 +18,15 @@ export default defineConfig({
       [
         'link',
         { rel: 'icon', href: '/favicon.svg' }
+      ],
+      [
+      'script',
+      { type: 'text/javascript' },
+      ` (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "t0p5gq619a");`
       ]
     ],
 
@@ -91,18 +100,7 @@ export default defineConfig({
       label: '本页目录'
     },
 
-    returnToTopLabel: '↑ 回到顶部',
+    returnToTopLabel: '↑ 回到顶部'
 
-    head: [
-      'script',
-      {
-        type: 'text/javascript',
-      },
-      `(function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "t0p5gq619a");`
-    ]
   }
 })
